d511554e9cd26769b268737bf20b29c619f5626e
vadimiowa <sytnykvadim740@gmail.com>
2024-10-10 00:54:12 +0300

    provide

--- script/animation.js
+++ script/animation.js
@@ -6,6 +6,50 @@ class AnimationElements {
         this.borderConta = options.borderConta;
         this.borderWhat = options.borderWhat;
         this.window = options.window;
+        this.groupTitle = options.groupTitle;
+    }
+
+    promise(sec) {
+        return new Promise(resolve => setTimeout(resolve, sec));
+    }
+
+    addClasses(node) {
+        node.classList.add("size");
+    }
+    removeClasses(node) {
+        node.classList.remove("size");
+    }
+
+    async process(el) {
+        await this.promise(200);
+        this.addClasses(el);
+    }
+    async processNot(el) {
+        await this.promise(200);
+        this.removeClasses(el);
+    }
+
+    async main() {
+        for(let el of this.groupTitle) {
+            await this.process(el)
+        }
+    }
+
+    async mainNone() {
+        for(let el of this.groupTitle) {
+            await this.processNot(el)
+        }
+    }
+
+    showMain() {
+        this.window.addEventListener("scroll", () => {
+            let scrolling = this.window.scrollY;
+            if(scrolling > 2900) {
+                this.main();
+            } else {
+                this.mainNone();
+            }
+        })
     }
 
     bottomAndtopAdd() {
@@ -25,13 +69,11 @@ class AnimationElements {
             } else {
                 this.border.classList.remove("activeBorder");
             }
-
             if(scroll > 1200) {
                 this.borderServi.classList.add("activeBorder");
             } else {
                 this.borderServi.classList.remove("activeBorder");
             }
-
             if(scroll > 1500) {
                 this.borderWhat.classList.add("activeBorderTop");
             } else {
@@ -42,13 +84,11 @@ class AnimationElements {
             } else {
                 this.borderWhat.classList.remove("activeBorderBottom");
             }
-
             if(scroll > 3300) {
                 this.borderConta.classList.add("activeBorder");
             } else {
                 this.borderConta.classList.remove("activeBorder");
             }
-
             // media
             if(scroll > 3600) {
                 this.borderConta.classList.remove("activeBorder");
@@ -91,7 +131,32 @@ let border = new AnimationElements({
     border: document.querySelector(".about__text"),
     borderServi: document.querySelector(".services__text"),
     borderConta: document.querySelector(".contactus__text"),
-    borderWhat: document.querySelector(".what")
+    borderWhat: document.querySelector(".what"),
 })
 
 border.radiusBorder();
+
+
+let groupTitleElements = new AnimationElements({
+    window: window,
+    groupTitle: document.querySelectorAll(".group__title")
+})
+
+
+groupTitleElements.showMain();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
