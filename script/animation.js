fda2c5b0a2c71282c77bb0205015c1fe5a388cfd
vadimiowa <sytnykvadim740@gmail.com>
2024-10-09 12:44:57 +0300

    allows only correct

--- script/animation.js
+++ script/animation.js
@@ -1,35 +1,73 @@
+// class AnimationElements {
+//     constructor(options) {
+//         this.mouse = options.mouse;
+//     }
+
+//     bottomAndtopAdd() {
+//         return this.mouse.classList.add("activeTop");
+//     }
+//     bottomAndtopRemove() {
+//         return this.mouse.classList.remove("activeTop");
+//     }
+// }
+
+// let mouse = new AnimationElements({
+//     mouse: document.querySelector(".correct__img")
+// })
+
+// let promiseFun = (one = 1000, two = 2000) => {
+//     let promise = new Promise(resolve => {
+//         setInterval(() => {
+//             resolve(mouse.bottomAndtopAdd());
+//         }, one)
+//         setInterval(() => {
+//             resolve(mouse.bottomAndtopRemove());
+//         }, two)
+//     })
+
+//     return promise;
+// }
+
+// let show = async () => {
+//     await promiseFun();
+// }
+
+// show();
+
 class AnimationElements {
     constructor(options) {
         this.mouse = options.mouse;
     }
 
     bottomAndtopAdd() {
-        return this.mouse.classList.add("activeTop");
+        this.mouse.classList.add("activeTop");
     }
+
     bottomAndtopRemove() {
-        return this.mouse.classList.remove("activeTop");
+        this.mouse.classList.remove("activeTop");
     }
 }
 
 let mouse = new AnimationElements({
     mouse: document.querySelector(".correct__img")
-})
+});
 
 let promiseFun = (one = 1000, two = 2000) => {
-    let promise = new Promise(resolve => {
-        setInterval(() => {
-            resolve(mouse.bottomAndtopAdd());
-        }, one)
-        setInterval(() => {
-            resolve(mouse.bottomAndtopRemove());
-        }, two)
-    })
-
-    return promise;
-}
+    return new Promise(resolve => {
+        setTimeout(() => {
+            mouse.bottomAndtopAdd();
+            setTimeout(() => {
+                mouse.bottomAndtopRemove();
+                resolve(); // Resolve the promise once the animation is complete
+            }, two);
+        }, one);
+    });
+};
 
 let show = async () => {
-    await promiseFun();
-}
+    while (true) { // Run indefinitely, adjust logic as needed
+        await promiseFun();
+    }
+};
 
 show();
